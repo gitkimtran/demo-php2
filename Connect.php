fa0b223bbbaec0cedfced9a2530fe94dcf5e89ae
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 09:43:23 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,27 +1,24 @@
 <?php 
 	$dbconn = pg_connect("host=ec2-52-20-248-222.compute-1.amazonaws.com port=5432 dbname=d6rknhl9m7oe3a user=yjrvedphbhercv password=e82de0cee9665a7ed23c792aad268603e607d79ad214e9c1f65279b712679a1a");
-	if (isset($_POST['username'])) {
-		# code...
-		$username = $_POST['username'];
-	}
+	$dbconn= pg_connect($conn_string);
+if (isset($_POST['username'])) {
+	# code...
+	$username = $_POST['username'];
+}
 
-	if (isset($_POST['pass'])) {
-		# code...
-		$pass = $_POST['pass']);
-	}
-
-	$sql = "SELECT * FROM tblaccount WHERE _user ='".$username."' and _pass = '".$pass."'";
-
-	$result = pg_query($dbconn, $sql);
-
-	$row = pg_num_rows($result);
-	
-	if ($row==1) {
-		# code...
-		echo "Login success";
-	}
-	else {
-		# code...
-		echo "Login failed";
-	}
+if (isset($_POST['pass'])) {
+	# code...
+	$pass = $_POST['pass'];
+}
+$sql = "SELECT * FROM tblAccount WHERE _user='".$username."' AND _pass='".$pass."'";
+$result = pg_query($dbconn, $sql);
+$row = pg_num_rows($result);
+if ($row==1)
+{
+	echo "Login success";
+}
+else
+{
+	echo "Login failed";
+}
  ?>
