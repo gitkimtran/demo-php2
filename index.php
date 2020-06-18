566ef5a2748e65bf3b9ebaedb9b9ca2ac9711b8e
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 09:11:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 	<form action="Connect.php" method="POST">
 		User:<br><input type="text" name="username" required="true"><br>
-		Password:<br><input type="password" name="pass" required="true"><br>
+		Password:<br><input type="password" name="pass" required="true"><br><br>
 		<input type="submit" name="" id="" value="Login">
 	</form>
 </body>
