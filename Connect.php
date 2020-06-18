1142c1ac6f5811e8bd473a8c54d2814bfd76e77a
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 09:04:32 +0700

    Add files via upload

---
+++ Connect.php
@@ -0,0 +1,26 @@
+<?php 
+	$connect = "host=ec2-52-20-248-222.compute-1.amazonaws.com port=5432 dbname=d6rknhl9m7oe3a user=yjrvedphbhercv password=e82de0cee9665a7ed23c792aad268603e607d79ad214e9c1f65279b712679a1a";
+	$dbconn = pg_connect($connect);
+	if (isset($_POST['username']) {
+		# code...
+		$username = $_POST['username'];
+	}
+
+	if (isset($_POST['pass'])) {
+		# code...
+		$pass = $_POST['pass']);
+	}
+
+	$sql = "select * from tblaccount where _user = '".$username."' and _pass = '".$pass."'";
+
+	$result = pg_query($sql, $dbconn);
+	$row = pg_num_rows($result);
+	if ($row==1) {
+		# code...
+		return "Login success";
+	}
+	else {
+		# code...
+		return "Login failed";
+	}
+ ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="Connect.php" method="POST">
+		<input type="text" name="username" label="User" required="true"><br>
+		<input type="password" name="pass" label="Pass" required="true"><br>
+		<input type="submit" name="" id="" value="Login">
+	</form>
+</body>
+</html>
