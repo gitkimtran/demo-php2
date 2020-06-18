5afc29f269e49c90ec59cc364e5158b9858b0795
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 09:19:25 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,7 +1,7 @@
 <?php 
-	$connect = "host=ec2-52-20-248-222.compute-1.amazonaws.com port=5432 dbname=d6rknhl9m7oe3a user=yjrvedphbhercv password=e82de0cee9665a7ed23c792aad268603e607d79ad214e9c1f65279b712679a1a";
-	$dbconn = pg_connect($connect);
-	if (isset($_POST['username']) {
+	$conn_string = "host=ec2-52-20-248-222.compute-1.amazonaws.com port=5432 dbname=d6rknhl9m7oe3a user=yjrvedphbhercv password=e82de0cee9665a7ed23c792aad268603e607d79ad214e9c1f65279b712679a1a";
+	$dbconn = pg_connect($conn_string);
+	if (isset($_POST['username'])) {
 		# code...
 		$username = $_POST['username'];
 	}
@@ -11,7 +11,7 @@
 		$pass = $_POST['pass']);
 	}
 
-	$sql = "SELECT * FROM tblaccount where _user = '".$username."' and _pass = '".$pass."'";
+	$sql = "SELECT * FROM tblaccount WHERE _user ='".$username."' and _pass = '".$pass."'";
 
 	$result = pg_query($dbconn, $sql);
 	$row = pg_num_rows($result);
