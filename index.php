dcb9a0b65eac5e84544f9b59343aa66dbdd30f3a
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 09:56:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Login Page</title>
 </head>
-<body>
-	<form action="Connect.php" method="POST">
-		User:<br><input type="text" name="username" required="true"><br>
-		Password:<br><input type="password" name="pass" required="true"><br><br>
-		<input type="submit" name="" id="" value="Login">
-	</form>
+<body >
+<<form action="Connect.php" method="POST" accept-charset="utf-8">
+Username:<input type="text" name="username" value="" placeholder="username"><br/>
+Password:<input type="password" name="pass" value="" placeholder="**********"><br/>
+<input type="submit" name="" value="Login"><br/>
+</form>
 </body>
 </html>
